285c86ec68be046a3f36fd16fb463bc2d0794914
larrykwon <koallarry11@kaist.ac.kr>
2021-12-20 23:47:37 +0900

    implement ImmGen

--- Lab3/template/ImmGen.v
+++ Lab3/template/ImmGen.v
@@ -27,15 +27,43 @@ always@(*) begin
     
     if(imm_control == 3'b010) begin
         //I-type and JALR() and L-type
+        offset[11:0] = I_MEM_DI[31:20];
+        if(I_MEM_DI[31]==1) begin
+            offset[31:12] = 20'b11111111111111111111;
+        end
+        else begin
+            offset[31:12] = 20'b00000000000000000000;
+        end
     end
     if(imm_control == 3'b011) begin
         //B-type
+        offset[0] = 0;
+        offset[4:1] = I_MEM_DI[11:8];
+        offset[10:5] = I_MEM_DI[30:25];
+        offset[11] = I_MEM_DI[7];
+        offset[12] = I_MEM_DI[31];
+        if(I_MEM_DI[31] == 1) begin
+            offset[31:13] = 20'b11111111111111111111;
+        end
+        else begin
+            offset[31:12] = 20'b00000000000000000000;
+        end
     end
     if(imm_control == 3'b100) begin
         //S-type
+        offset[4:0] = I_MEM_DI[11:7];
+        offset[11:5] = I_MEM_DI[31:25];
+        if(I_MEM_DI[31] == 1) begin
+            offset[31:12] = 20'b11111111111111111111;
+        end
+        else begin
+            offset[31:12] = 20'b00000000000000000000;
+        end
     end
     if(imm_control == 3'b101) begin
         //Shift, SLLI, SRLI, SRAI
+        offset[4:0] = I_MEM_DI[24:20];
+        offset[31:5] = 27'b000000000000000000000000000;
     end
 end
 
