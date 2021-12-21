fcce4af2e130c3f52c85caaf4bc96d728051e666
larrykwon <koallarry11@kaist.ac.kr>
2021-12-21 23:07:39 +0900

    ControlUnit 구현중 alu_control, wb_control 아직 앙ㄴ함. BrEq,과 BrLt에 따라서 wb_control 값 바껴야함. SW할 때 writedata에 Rd2값을 연결해야하는데 그걸 아직 안했음.

--- Lab3/template/ControlUnit.v
+++ Lab3/template/ControlUnit.v
@@ -1,22 +1,29 @@
 module ControlUnit(
     input wire RSTn,
     input wire [31:0] I_MEM_DI,
-    input wire BrEq,
-    input wire BrLt,
-    output wire [2:0] imm_control,
-    output wire RF_WE,
-    output wire D_MEM_WEN,
-    output wire [3:0] D_MEM_BE,
-    output wire is_sign,
-    output wire BSel,
-    output wire ASel,
-    output wire [4:0] alu_control,
-    output wire [2:0] wb_control
+    input wire BrEq, // 아직 처리 X
+    input wire BrLt, // 아직 처리 X
+    output wire [2:0] imm_control, // 처리 O
+    output wire RF_WE, // 처리 O
+    output wire D_MEM_WEN, // 처리 O
+    output wire [3:0] D_MEM_BE, // 처리 O
+    output wire is_sign, // 처리 O
+    output wire ASel, // 처리 O // 1이면 pc 0이면 RF_RD1
+    output wire BSel, // 처리 O // 1이면 imm 0이면 RF_RD2
+    output wire [4:0] alu_control, // 처리 X
+    output wire [2:0] wb_control // 처리 X
 );
 
 reg[6:0] opcode;
 reg[2:0] func3;
 reg[6:0] func7;
+
+// reg RF_WE_Reg;
+// reg D_MEM_WEN_Reg;
+// reg [3:0] D_MEM_BE_Reg;
+
+// assign RF_WE = RF_WE_Reg;
+// assign D_MEM_WEN = D_MEM_WEN_Reg;
 
 //control
 reg[6:0] op_LUI;
@@ -39,6 +46,9 @@ initial begin
     op_Stype <= 7'b0100011;
     op_Itype <= 7'b0010011;
     op_Rtype <= 7'b0110011;
+
+    RF_WE <= 0;
+    D_MEM_WEN <= 1;
 end
 
 
@@ -47,37 +57,131 @@ always@(*) begin
         opcode = I_MEM_DI[6:0];
         func3 = I_MEM_DI[14:12];
         func7 = I_MEM_DI[31:25];
+
         if(opcode == op_LUI) begin
-            imm_control = 3'b000;
+            imm_control = 3'b000; // U-type imm 처리
+            RF_WE = 1; // imm을 rd에 써야함
+            D_MEM_WEN = 1; // 메모리에 쓸 필요 없음
+            //ALU연산 필요 없음
+            
         end
         if(opcode == op_AUIPC) begin
-            imm_control = 3'b000;
+            imm_control = 3'b000; // U-type imm 처리, 20비트 읽기
+            RF_WE = 1;  // 연산결과 rd에 써야함
+            D_MEM_WEN = 1; // 메모리에 쓸 필요 없음
+            //ALU: pc + imm
+            ASel = 1;
+            BSel = 1;
         end
         if(opcode == op_Rtype) begin
-
-        end
-        if(opcode == op_Itype) begin
-            if(func3 = 3'b101 || func3 = 3'b001) begin
-                imm_control = 3'b101;
+            // imm이 없음
+            RF_WE = 1; // 연산결과 rd에 써야함
+            D_MEM_WEN = 1; // 메모리에 쓸 필요 없음
+            //ALU: rd1 + rd2
+            ASel = 0;
+            BSel = 0;
+            if(func == 3'b011) begin
+                is_sign = 0;
             end
             else begin
-                imm_control = 3'b010;
+                is_sign = 1;
             end
         end
-        if(opcode == op_Ltype) begin
-            imm_control = 3'b010;
+        if(opcode == op_Itype) begin
+            RF_WE = 1; // 연산결과 rd에 써야함
+            D_MEM_WEN = 1; // 메모리에 쓸 필요 없음
+            //ALU: rd1 + imm
+            ASel = 0;
+            BSel = 1;
+            if(func3 == 3'b101 || func3 == 3'b001) begin
+                imm_control = 3'b101; // shift 연산시 imm은 shamt
+            end
+            else begin
+                imm_control = 3'b010; // 그 외에는 12비트 읽기
+            end
+            
+            if(func == 3'b011) begin
+                is_sign = 0;
+            end
+            else begin
+                is_sign = 1;
+            end
         end
+
+        if(opcode == op_Ltype) begin
+            imm_control = 3'b010; // 상위 12비트 그냥 읽기
+            RF_WE = 1; // 연산결과 rd에 써야함
+            D_MEM_WEN = 1; // 메모리에 쓸 필요 없음
+            //ALU: rd1 + imm
+            ASel = 0;
+            BSel = 1;
+            if(func == 3'b100 || func == 3'b101) begin
+                is_sign = 0;
+            end
+            else begin
+                is_sign = 1;
+            end
+
+            //D_MEM_BE
+            if(func == 3'b000 || func == 3'b100) begin
+                D_MEM_BE = 3'b0001;
+            end
+            else if(func == 3'b001 || func == 3'b101) begin
+                D_MEM_BE = 3'b0011;
+            end
+            else begin
+                D_MEM_BE = 3'b1111;
+            end
+        end
+
         if(opcode == op_Stype) begin
-            imm_control = 3'b100;
+            imm_control = 3'b100; // S-type, 잘라서 12비트 만들기
+            RF_WE = 0; // 연산결과 rd에 안써도 됨.
+            D_MEM_WEN = 0; // 메모리에 데이터 써야함.
+            //ALU: rd1 + imm
+            ASel = 0;
+            BSel = 1;
+
+            //D_MEM_BE
+            if(func == 3'b000) begin
+                D_MEM_BE = 3'b0001;
+            end
+            else if(func == 3'b001) begin
+                D_MEM_BE = 3'b0011;
+            end
+            else begin
+                D_MEM_BE = 3'b1111;
+            end
         end
         if(opcode == op_Btype) begin
-            imm_control = 3'b011;
+            imm_control = 3'b011; // B-type 잘라서 13비트 만들기
+            RF_WE = 0; // 연산결과 rd에 안써도 됨.
+            D_MEM_WEN = 1; // 메모리에 쓸 필요 없음
+            //ALU: pc + imm;
+            ASel = 1;
+            BSel = 1;
+            if(func == 3'b110 || func == 3'b111) begin
+                is_sign = 0;
+            end
+            else begin
+                is_sign = 1;
+            end
         end
         if(opcode == op_JALR) begin
-            imm_control = 3'b010;
+            imm_control = 3'b010; // 상위 12비트 읽기
+            RF_WE = 1; // pc+4를 rd에 저장
+            D_MEM_WEN = 1; // 메모리에 쓸 필요 없음
+            //ALU: rd1 + imm
+            ASel = 0;
+            BSel = 1;
         end
         if(opcode == op_JAL) begin
-            imm_control = 3'001;
+            imm_control = 3'001; // 잘라서 21비트 읽기
+            RF_WE = 1; // pc+4를 rd에 저장
+            D_MEM_WEN = 1; // 메모리에 쓸 필요 없음
+            //ALU: pc + imm
+            ASel = 1;
+            BSel = 1;
         end
     end
 end
